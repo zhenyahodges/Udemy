338acc0cf8484606aa704baa060cbeba0932a2e0
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-12-11 23:45:04 +0000

    list data dynamically

--- react-udemy/01-starting-project/src/App.jsx
+++ react-udemy/01-starting-project/src/App.jsx
@@ -32,25 +32,36 @@ function App() {
                 <section id='core-concepts'>
                     <h2>Core Concepts</h2>
                     <ul>
-                        <CoreConcept {...CORE_CONCEPTS[0]} />
-                        <CoreConcept {...CORE_CONCEPTS[1]} />
-                        <CoreConcept {...CORE_CONCEPTS[2]} />
-                        <CoreConcept {...CORE_CONCEPTS[3]} />
+                        {CORE_CONCEPTS.map((conceptItem) => (
+                            <CoreConcept {...conceptItem} />
+                        ))}
                     </ul>
                 </section>
                 <section id='examples'>
                     <h2>Examples</h2>
                     <menu>
-                        <TabButton isSelected={selectedTopic==='components'} onSelect={() => handleSelect('components')}>
+                        <TabButton
+                            isSelected={selectedTopic === 'components'}
+                            onSelect={() => handleSelect('components')}
+                        >
                             Components
                         </TabButton>
-                        <TabButton isSelected={selectedTopic==='jsx'} onSelect={() => handleSelect('jsx')}>
+                        <TabButton
+                            isSelected={selectedTopic === 'jsx'}
+                            onSelect={() => handleSelect('jsx')}
+                        >
                             JSX
                         </TabButton>
-                        <TabButton isSelected={selectedTopic==='props'} onSelect={() => handleSelect('props')}>
+                        <TabButton
+                            isSelected={selectedTopic === 'props'}
+                            onSelect={() => handleSelect('props')}
+                        >
                             Props
                         </TabButton>
-                        <TabButton isSelected={selectedTopic==='state'} onSelect={() => handleSelect('state')}>
+                        <TabButton
+                            isSelected={selectedTopic === 'state'}
+                            onSelect={() => handleSelect('state')}
+                        >
                             State
                         </TabButton>
                     </menu>
