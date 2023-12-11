0b255e022beeb8d4ce6f4e209425166050e142ba
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-12-11 21:40:05 +0000

    update css and  folders

--- react-udemy/01-starting-project/src/App.jsx
+++ react-udemy/01-starting-project/src/App.jsx
@@ -1,5 +1,5 @@
 import CoreConcept from './components/CoreConcept';
-import Header from './components/Header';
+import Header from './components/Header/Header';
 import { CORE_CONCEPTS } from './data';
 
 function App() {
