fb8f059b256be4ec212fb52682b3217b9a0f31eb
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-12-11 21:47:03 +0000

    btns

--- react-udemy/01-starting-project/src/App.jsx
+++ react-udemy/01-starting-project/src/App.jsx
@@ -1,5 +1,6 @@
 import CoreConcept from './components/CoreConcept';
 import Header from './components/Header/Header';
+import TabButton from './components/TabButton';
 import { CORE_CONCEPTS } from './data';
 
 function App() {
@@ -10,11 +11,20 @@ function App() {
                 <section id='core-concepts'>
                     <h2>Core Concepts</h2>
                     <ul>
-                      <CoreConcept {...CORE_CONCEPTS[0]} />
-                      <CoreConcept {...CORE_CONCEPTS[1]} />
-                      <CoreConcept {...CORE_CONCEPTS[2]} />
-                      <CoreConcept {...CORE_CONCEPTS[3]} />
+                        <CoreConcept {...CORE_CONCEPTS[0]} />
+                        <CoreConcept {...CORE_CONCEPTS[1]} />
+                        <CoreConcept {...CORE_CONCEPTS[2]} />
+                        <CoreConcept {...CORE_CONCEPTS[3]} />
                     </ul>
+                </section>
+                <section id='examples'>
+                    <h2>Examples</h2>
+                    <menu>
+                      <TabButton>Components</TabButton>
+                      <TabButton>JSX</TabButton>
+                      <TabButton>Props</TabButton>
+                      <TabButton>State</TabButton>
+                    </menu>
                 </section>
             </main>
         </div>
