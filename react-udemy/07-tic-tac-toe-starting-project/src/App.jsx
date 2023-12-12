df2bb6d06eeb8803a6b6aa567a687cd36b3c7816
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-12-12 21:55:16 +0000

    sharing state across comps

--- react-udemy/07-tic-tac-toe-starting-project/src/App.jsx
+++ react-udemy/07-tic-tac-toe-starting-project/src/App.jsx
@@ -53,7 +53,7 @@ function App() {
                     turns={gameTurns}
                 />
             </div>
-            <Log />
+            <Log turns={gameTurns}/>
         </main>
     );
 }
