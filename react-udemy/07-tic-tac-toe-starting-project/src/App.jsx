c269aaedb369ff980e2a73f6068177ba16441458
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-12-12 20:15:31 +0000

    prep

--- react-udemy/07-tic-tac-toe-starting-project/src/App.jsx
+++ react-udemy/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,6 +1,18 @@
+import Player from './components/PLayer';
+
 function App() {
     return (
-    <p>Coming soon</p>
+        <main>
+            <div id='game-container'>
+                <ol id='players'>
+                    
+                    <Player name='Player 1' symbol='X'/>
+                    <Player name='Player 2' symbol='O'/>
+                </ol>
+                game board
+            </div>
+            log
+        </main>
     );
 }
 
