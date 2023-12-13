a6642c1b312e624a9da48d2a1bcd8acd59607eee
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-12-13 19:28:09 +0000

    altern to lifting state up

--- react-udemy/07-tic-tac-toe-starting-project/src/App.jsx
+++ react-udemy/07-tic-tac-toe-starting-project/src/App.jsx
@@ -55,7 +55,7 @@ function App() {
             firstSquareSymbol === secondSquareSymbol &&
             firstSquareSymbol === thirdSquareSymbol
         ) {
-            winner = firstSquareSymbol;
+            winner = players[firstSquareSymbol];
         }
     }
 
@@ -104,11 +104,13 @@ function App() {
                         initialName='Player 1'
                         symbol='X'
                         isActive={activePlayer === 'X'}
+                        onChangeName={handlePlayerNameChange}
                     />
                     <Player
                         initialName='Player 2'
                         symbol='O'
                         isActive={activePlayer === 'O'}
+                        onChangeName={handlePlayerNameChange}
                     />
                 </ol>
                 {(winner || hasDraw) && (
