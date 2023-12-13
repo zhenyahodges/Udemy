2f0544968cd991d2fc858e97f494e02dbd2bfd80
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-12-13 14:20:15 +0000

    Update App.jsx
    when not to lift state up

--- react-udemy/07-tic-tac-toe-starting-project/src/App.jsx
+++ react-udemy/07-tic-tac-toe-starting-project/src/App.jsx
@@ -21,6 +21,11 @@ function derivedActivePlayer(gameTurns) {
 }
 
 function App() {
+    const [players, setPlayers] = useState({
+        X: 'Player 1',
+        O: 'Player 2',
+    });
+
     const [gameTurns, setGameTurns] = useState([]);
     // const [activePlayer, setActivePlayer] = useState('X');
 
@@ -80,6 +85,15 @@ function App() {
 
     function handleRestart() {
         setGameTurns([]);
+    }
+
+    function handlePlayerNameChange(symbol, newName) {
+        setPlayers((prevPlayers) => {
+            return {
+                ...prevPlayers,
+                [symbol]: newName,
+            };
+        });
     }
 
     return (
