ee9ccf6d0409ecbfe4bbdd42e7fa0c14e91c000c
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-12-13 08:50:34 +0000

    reducing state mnt and unnecess state

--- react-udemy/07-tic-tac-toe-starting-project/src/App.jsx
+++ react-udemy/07-tic-tac-toe-starting-project/src/App.jsx
@@ -3,22 +3,29 @@ import GameBoard from './components/GameBoard';
 import Player from './components/PLayer';
 import Log from './components/Log';
 
+function derivedActivePlayer(gameTurns) {
+    let currentPlayer = 'X';
+
+    if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+        currentPlayer = 'O';
+    }
+    return currentPlayer;
+}
+
 function App() {
     const [gameTurns, setGameTurns] = useState([]);
-    const [activePlayer, setActivePlayer] = useState('X');
+    // const [activePlayer, setActivePlayer] = useState('X');
+
+    const activePlayer = derivedActivePlayer(gameTurns);
 
     function handleSelectSquare(rowIndex, colIndex) {
-        setActivePlayer((curActivePlayer) =>
-            curActivePlayer === 'X' ? 'O' : 'X'
-        );
+        // setActivePlayer((curActivePlayer) =>
+        //     curActivePlayer === 'X' ? 'O' : 'X'
+        // );
 
         setGameTurns((prevTurns) => {
-            let currentPlayer = 'X';
-
-            if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
-                currentPlayer = 'O';
-            }
-
+           const currentPlayer= derivedActivePlayer(prevTurns);
+           
             const updatedTurns = [
                 {
                     square: {
@@ -53,7 +60,7 @@ function App() {
                     turns={gameTurns}
                 />
             </div>
-            <Log turns={gameTurns}/>
+            <Log turns={gameTurns} />
         </main>
     );
 }
