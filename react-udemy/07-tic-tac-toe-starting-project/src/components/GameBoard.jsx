7c62b3e0554e147637b3556a41bf503c71f7f3f4
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-12-13 08:54:23 +0000

    disabling btns conditionally

--- react-udemy/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ react-udemy/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -5,7 +5,7 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard({ onSelectSquare, turns }) {
-    //NB deriving state! try to derive as much info and as many values as poss from that state! 
+    //NB deriving state! try to derive as much info and as many values as poss from that state!
     // & manage as little state as needed
     // computed value derived from some state, i.e.game turns in app component
     let gameBoard = initialGameBoard;
@@ -38,7 +38,12 @@ export default function GameBoard({ onSelectSquare, turns }) {
                     <ol>
                         {row.map((playerSymbol, columnIndex) => (
                             <li key={columnIndex}>
-                                <button onClick={()=>onSelectSquare(rowIndex,columnIndex)}>
+                                <button
+                                    onClick={() =>
+                                        onSelectSquare(rowIndex, columnIndex)
+                                    }
+                                    disabled={playerSymbol !== null}
+                                >
                                     {playerSymbol}
                                 </button>
                             </li>
