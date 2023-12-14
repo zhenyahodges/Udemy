b8fe008d2fa63d737ccc6afa84ecdaafee2e0788
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-12-14 17:46:39 +0000

    Update UserInput.jsx

--- react-udemy/07-05-essential-practice/src/components/UserInput.jsx
+++ react-udemy/07-05-essential-practice/src/components/UserInput.jsx
@@ -1,4 +1,22 @@
+import { useState } from 'react';
+
 export default function UserInput() {
+    const [userInput, setUserInput] = useState({
+        initialInvestment: 10000,
+        annualInvestment: 1200,
+        expectedReturn: 6,
+        duration: 10,
+    });
+
+    function handleChange(inputIdentifier, newValue) {
+        setUserInput((prevUserInput) => {
+            return {
+                ...prevUserInput,
+                [inputIdentifier]: newValue,
+            };
+        });
+    }
+
     return (
         <section id='user-input'>
             <article className='input-group'>
