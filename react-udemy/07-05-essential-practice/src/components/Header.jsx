8a811343917124f40d230ff9846cb46c807cf16f
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-12-14 17:36:13 +0000

    Update Header.jsx

--- react-udemy/07-05-essential-practice/src/components/Header.jsx
+++ react-udemy/07-05-essential-practice/src/components/Header.jsx
@@ -1,7 +1,9 @@
+import logo from '../assets/investment-calculator-logo.png';
+
 export default function Header(){
     return(
         <header id='header'>
-            <img src='../src/assets/investment-calculator-logo.png' alt='investment-calculator-logo' />
+            <img src={logo} alt='investment-calculator-logo' />
             <h1 id='header'>Investment Calculator</h1>
         </header>
     );
