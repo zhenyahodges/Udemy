4406218d5f9a373c244929e5bf63e3ab5b3f1d1d
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-12-15 00:24:22 +0000

    Update AuthInputs.jsx

--- react-udemy/07-styling/src/components/AuthInputs.jsx
+++ react-udemy/07-styling/src/components/AuthInputs.jsx
@@ -9,24 +9,25 @@ const ControlDiv = styled.div`
 `;
 
 const Label = styled.label`
-display: block;
-margin-bottom: 0.5rem;
-font-size: 0.75rem;
-font-weight: 700;
-letter-spacing: 0.1em;
-text-transform: uppercase;
-color: ${({invalid})=>invalid? '#f87171':' #6b7280'};
+    display: block;
+    margin-bottom: 0.5rem;
+    font-size: 0.75rem;
+    font-weight: 700;
+    letter-spacing: 0.1em;
+    text-transform: uppercase;
+    color: ${({ $invalid }) => ($invalid ? '#f87171' : '#6b7280')};
 `;
 
-const Input=styled.input`
-width: 100%;
-  padding: 0.75rem 1rem;
-  line-height: 1.5;
-  background-color: #d1d5db;
-  color: #374151;
-  border: 1px solid transparent;
-  border-radius: 0.25rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+const Input = styled.input`
+    width: 100%;
+    padding: 0.75rem 1rem;
+    line-height: 1.5;
+    background-color: ${({ $invalid }) => ($invalid ? '#fed2d2' : '#d1d5db')};
+    color: ${({ $invalid }) => ($invalid ? '#ef4444' : '#374151')};
+    border: 1px solid
+        ${({ $invalid }) => ($invalid ? '#f73f3f' : 'transparent')};
+    border-radius: 0.25rem;
+    box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
 `;
 
 export default function AuthInputs() {
@@ -54,29 +55,31 @@ export default function AuthInputs() {
             <ControlDiv>
                 <p>
                     <Label
-                    invalid={emailNotValid}
-                    // className={`label ${emailNotValid ? 'invalid' : ''}`} 
+                        $invalid={emailNotValid}
+                        // className={`label ${emailNotValid ? 'invalid' : ''}`}
                     >
                         Email
                     </Label>
                     <Input
+                        $invalid={emailNotValid}
                         type='email'
-                        style={{
-                            backgroundColor: emailNotValid
-                                ? '#fed2d2'
-                                : '#d1d5db',
-                        }}
-                        className={emailNotValid ? 'invalid' : undefined}
-                        // onChange={(event) =>
-                        //     handleInputChange('email', event.target.value)
-                        // }
+                        // style={{
+                        //     backgroundColor: emailNotValid
+                        //         ? '#fed2d2'
+                        //         : '#d1d5db',
+                        // }}
+                        // className={emailNotValid ? 'invalid' : undefined}
+                        onChange={(event) =>
+                            handleInputChange('email', event.target.value)
+                        }
                     />
                 </p>
                 <p>
-                    <Label>Password</Label>
+                    <Label $invalid={passwordNotValid}>Password</Label>
                     <Input
                         type='password'
-                        className={passwordNotValid ? 'invalid' : undefined}
+                        $invalid={passwordNotValid}
+                        // className={passwordNotValid ? 'invalid' : undefined}
                         onChange={(event) =>
                             handleInputChange('password', event.target.value)
                         }
