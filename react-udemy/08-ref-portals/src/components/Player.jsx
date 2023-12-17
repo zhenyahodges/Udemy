6c24b23f6b1559c7b54b1931dd29a201139d082b
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-12-17 10:29:29 +0000

    using state

--- react-udemy/08-ref-portals/src/components/Player.jsx
+++ react-udemy/08-ref-portals/src/components/Player.jsx
@@ -1,11 +1,25 @@
+import { useState } from 'react';
+
 export default function Player() {
-  return (
-    <section id="player">
-      <h2>Welcome unknown entity</h2>
-      <p>
-        <input type="text" />
-        <button>Set Name</button>
-      </p>
-    </section>
-  );
+    const [player, setPlayer] = useState('');
+    const [submitted, setSubmitted] = useState(false);
+
+    function handleChange(e) {
+        setSubmitted(false);
+        setPlayer(e.target.value);
+    }
+
+    function handleClick() {
+        setSubmitted(true);
+    }
+
+    return (
+        <section id='player'>
+            <h2>Welcome {submitted ? player : 'unknown entity'}</h2>
+            <p>
+                <input type='text' onChange={handleChange} value={player} />
+                <button onClick={handleClick}>Set Name</button>
+            </p>
+        </section>
+    );
 }
