e1b99d2a1c7f093d4bc3b1b17614343f7542817f
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-12-17 14:05:59 +0000

    expose comp apis via useImperative

--- react-udemy/08-ref-portals/src/components/ResultModal.jsx
+++ react-udemy/08-ref-portals/src/components/ResultModal.jsx
@@ -1,8 +1,21 @@
-import { forwardRef } from 'react';
+import { forwardRef, useImperativeHandle, useRef } from 'react';
 
-const ResultModal= forwardRef(function ResultModal({result, targetTime },ref) {
+const ResultModal = forwardRef(function ResultModal(
+    { result, targetTime },
+    ref
+) {
+    const dialog = useRef();
+
+    useImperativeHandle(ref, () => {
+        return {
+            open() {
+                dialog.current.showModal();
+            },
+        };
+    });
+
     return (
-        <dialog ref={ref} className='result-modal'>
+        <dialog ref={dialog} className='result-modal'>
             <h2>You {result}</h2>
             <p>
                 The target time was <stron>{targetTime} </stron>seconds
@@ -15,6 +28,6 @@ const ResultModal= forwardRef(function ResultModal({result, targetTime },ref) {
             </form>
         </dialog>
     );
-}) ;
+});
 
 export default ResultModal;
