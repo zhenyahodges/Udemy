2ab3deb08c3208e12e77fdee5e5ed738c5659296
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2024-01-29 23:04:43 +0000

    Update app.module.ts

--- Angular-udemy/my-first-app/src/app/app.module.ts
+++ Angular-udemy/my-first-app/src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { FormsModule } from '@angular/forms';
+// import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    // HttpModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
