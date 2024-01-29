50e907784d1cd97b943cfecc87a60e804fcf71e4
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2024-01-29 23:17:27 +0000

    styles

--- Angular-udemy/my-first-app/src/app/app.component.ts
+++ Angular-udemy/my-first-app/src/app/app.component.ts
@@ -4,7 +4,12 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  // styles: [
+  //   `
+  //     h3 {
+  //       color: yellow;
+  //     }
+  //   `,
+  // ],
 })
-export class AppComponent {
-
-}
+export class AppComponent {}
