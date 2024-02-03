b8570a15f8f6d9f26a4d316ec3311aff69e192e9
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2024-02-03 13:23:26 +0000

    combining all binding

--- Angular-udemy/my-first-app/src/app/servers/servers.component.ts
+++ Angular-udemy/my-first-app/src/app/servers/servers.component.ts
@@ -22,7 +22,7 @@ export class ServersComponent {
   ngOnInit() {}
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created';
+    this.serverCreationStatus = 'Server was created! Name is: ' + this.serverName;
   }
 
   onUpdateServerName(event: any) {
