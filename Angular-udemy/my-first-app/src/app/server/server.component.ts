6b7ca2b7e1b39bf5aba0872f59beea747d60947d
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2024-02-03 18:47:46 +0000

    attribute directives, styling comps with ngStyle

--- Angular-udemy/my-first-app/src/app/server/server.component.ts
+++ Angular-udemy/my-first-app/src/app/server/server.component.ts
@@ -1,15 +1,22 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-server',
-    templateUrl: './server.component.html'
+  selector: 'app-server',
+  templateUrl: './server.component.html',
 })
-export class ServerComponent{
-    
-    serverId=10;
-    serverStatus:string='offline';
+export class ServerComponent {
+  serverId = 10;
+  serverStatus: string = 'offline';
 
-    // getServerStatus(){
-    //     return this.serverStatus;
-    // }
+  constructor(){
+    this.serverStatus=Math.random()> 0.5? 'online' : 'offline';
+  }
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+
+  getColor(){
+    return this.serverStatus=='online' ? 'green' : 'red';
+  }
 }
