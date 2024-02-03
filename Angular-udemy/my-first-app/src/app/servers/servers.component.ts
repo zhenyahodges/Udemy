b2527d66f4240ce31197e225d16e47a1d4dc2a1b
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2024-02-03 13:21:07 +0000

    two-way data binding

--- Angular-udemy/my-first-app/src/app/servers/servers.component.ts
+++ Angular-udemy/my-first-app/src/app/servers/servers.component.ts
@@ -11,7 +11,7 @@ import { Component } from '@angular/core';
 export class ServersComponent {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
-  serverName = '';
+  serverName = 'Test';
 
   constructor() {
     setTimeout(() => {
