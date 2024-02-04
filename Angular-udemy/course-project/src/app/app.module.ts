bc4ca029baa9582d4107e3941c13593bea7e8c15
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2024-02-04 22:51:40 +0000

    header and recipies comps

--- Angular-udemy/course-project/src/app/app.module.ts
+++ Angular-udemy/course-project/src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
-
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipiesComponent } from './recipies/recipies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipiesComponent
   ],
   imports: [
     BrowserModule,
---
+++ Angular-udemy/course-project/src/app/recipies/recipies.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipies',
+  templateUrl: './recipies.component.html',
+  styleUrl: './recipies.component.css'
+})
+export class RecipiesComponent {
+
+}
