d3c45dcb99b7677153f99b978bc8709efdef549d
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2024-02-05 16:04:46 +0000

    Update recipes-list.component.ts

--- Angular-udemy/course-project/src/app/recipes/recipes-list/recipes-list.component.ts
+++ Angular-udemy/course-project/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -7,13 +7,13 @@ import { Recipe } from '../recipes.model';
   styleUrl: './recipes-list.component.css'
 })
 export class RecipesListComponent {
-  recipes= Recipe[] = [
-    new Recipe('A Test Recipe', 'This is a test', 'https://unsplash.com/photos/sliced-tomatoes-with-ground-pork-iNwCO9ycBlc')
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a test', 'https://images.unsplash.com/photo-1518779578993-ec3579fee39f?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')
   ];
 
-  constructor(){
+  // constructor(){
 
-  }
+  // }
 
 
 }
