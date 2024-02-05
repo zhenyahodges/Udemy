769cc6008e13de8809ff2e6d62b0266b27aa0f1a
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2024-02-05 15:04:39 +0000

    components creation

---
+++ Angular-udemy/course-project/src/app/recipes/recipes-detail/recipes-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipes-detail',
+  templateUrl: './recipes-detail.component.html',
+  styleUrl: './recipes-detail.component.css'
+})
+export class RecipesDetailComponent {
+
+}
---
+++ Angular-udemy/course-project/src/app/recipes/recipes-list/recipes-item/recipes-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipes-item',
+  templateUrl: './recipes-item.component.html',
+  styleUrl: './recipes-item.component.css'
+})
+export class RecipesItemComponent {
+
+}
